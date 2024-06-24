4fe5eb79a71e36d9e4e63aa974dae23f82cdcf8c
Mehmet Ali Çuhacı <mehmet.cuhaci10@gmail.com>
2024-06-24 21:27:07 +0300

    .

--- SurveyMicroServices/obj/Release/net8.0/SurveyMicroServices.AssemblyInfo.cs
+++ SurveyMicroServices/obj/Release/net8.0/SurveyMicroServices.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyMicroServices")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918c3a18c55b843992497a6ba9582438240042ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyMicroServices")]
