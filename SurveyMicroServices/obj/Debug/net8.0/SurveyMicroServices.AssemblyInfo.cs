2deac35b6fea4398c6eba0fb88c8af6c0168d68d
mali <mehmet.cuhaci34@hotmail.com>
2024-06-27 14:07:26 +0300

    Fixed some bugs

--- SurveyMicroServices/obj/Debug/net8.0/SurveyMicroServices.AssemblyInfo.cs
+++ SurveyMicroServices/obj/Debug/net8.0/SurveyMicroServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyMicroServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa619f6dde20b243d953d22f607d20560548a3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a73b88ebab209fdfc929125c4deef64cf59266")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyMicroServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyMicroServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
